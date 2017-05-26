f22bdd453dcc8568365ca2c183616ec7e17472a8
marcus <marcus@MarxKitBook.scc.kit.edu>
2017-05-26 15:45:41 +0200

    map is back - but with too many dependencies

--- base_app/static/base_app/js/base.js
+++ base_app/static/base_app/js/base.js
@@ -36,7 +36,7 @@ function search_close(){
 function search_open(){
 	if (!document.getElementById("search_box")){
 		var searchBox = document.getElementById("srch_box");
-		searchBox.outerHTML = "<a class='respo-hover-none' style='height:10px' id='search_box'><input type='search' value='' placeholder='Search ...' style='height:15px; font-size:75%;'></a>";
+		searchBox.outerHTML = "<a class='respo-hover-none' style='height:10px' id='search_box'><input type='search' value='' placeholder='Search ...' style='height:10px; font-size:75%;'></a>";
 
 		var searchBut = document.getElementById("srch_but");
 		searchBut.outerHTML = "<a href='#' class='respo-hover-white' style='height:80px' id='search_but' onclick='search_close()'><i class='fa fa-search fa-fw'></i></a>";
@@ -50,9 +50,9 @@ function search_open(){
 function select_data(evt) {
 	var files = evt.target.files;
 	for (var i = 0, f; f = files[i]; i++) {
-		document.getElementById("workspace").innerHTML += "<li class='respo-padding' " +
-		"id='"+f.name+"'><span class='respo-medium'><strong>"+f.name+
-		"</strong> \r\n("+ f.type + ") - "+f.size+"  bytes</span><a href='javascript:void(0)' onclick=this.parentElement.remove(); " +
+		document.getElementById("workspace").innerHTML += "<li class='respo-padding' " +"id='"+f.name+
+		"'><span class='respo-medium'><strong>"+f.name+"</strong> \r\n("+ f.type + ") - "+f.size+
+ 		"  bytes</span><a href='javascript:void(0)' onclick=this.parentElement.remove(); " +
 		"class='respo-hover-white respo-right'><i class='fa fa-remove fa-fw'></i></a><br></li>";
 		}
 
