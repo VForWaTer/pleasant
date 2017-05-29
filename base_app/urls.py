beedb2b29c62fc20bea4aa4acd318836dc409e57
marcus <marcus@MarxKitBook.scc.kit.edu>
2017-05-29 16:00:31 +0200

    Ldap Login - not working

--- base_app/urls.py
+++ base_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'base_app'
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^login$', views.Login.as_view(), {'template_name': 'base_app/login.html'}, name='login'),
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import TemplateView
 
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'base_app/home.html'
 
+
+class Login(TemplateView):
+    template_name = 'base_app/login.html'
