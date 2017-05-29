d78647eda289c862a58b082449bc51e07895f911
marcus <marcus@MarxKitBook.scc.kit.edu>
2017-05-29 17:27:17 +0200

    correct position for search box

--- base_app/static/base_app/js/base.js
+++ base_app/static/base_app/js/base.js
@@ -9,13 +9,13 @@ function search_close(){
 function search_open(){
 	if (!document.getElementById("search_box")){
 		var searchBox = document.getElementById("srch_box");
-		searchBox.outerHTML = "<a class='respo-hover-none' style='height:10px' id='search_box'><input type='search' value='' placeholder='Search ...' style='height:10px; font-size:75%;'></a>";
+		searchBox.outerHTML = "<a class='respo-hover-none' style='height:1px' id='search_box'><input type='search' value='' placeholder='Search ...' style='height:0px; font-size:85%; margin-top:-6px;'></a>";
 
 		var searchBut = document.getElementById("srch_but");
-		searchBut.outerHTML = "<a href='#' class='respo-hover-white' style='height:80px' id='search_but' onclick='search_close()'><i class='fa fa-search fa-fw'></i></a>";
+		searchBut.outerHTML = "<a href='#' class='respo-hover-white' style='height:0px' id='search_but' onclick='search_close()'><i class='fa fa-search fa-fw'></i></a>";
 
 		var closeBut = document.getElementById("srch_close_but");
-		closeBut.outerHTML = "<a href='javascript:void(0)' class='respo-hover-white' style='height:80px' id='search_close_but' onclick='search_close()'><i class='fa fa-remove fa-fw'></i></a>";
+		closeBut.outerHTML = "<a href='javascript:void(0)' class='respo-hover-white' style='height:0px' id='search_close_but' onclick='search_close()'><i class='fa fa-remove fa-fw'></i></a>";
 	}
 }
 
