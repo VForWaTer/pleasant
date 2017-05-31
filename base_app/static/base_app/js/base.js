6a07ab3ceeab8c4d3f9aaf452c70e57cd05f9bc2
marcus <marcus@MarxKitBook.scc.kit.edu>
2017-05-31 11:30:03 +0200

    small fix on search button

--- base_app/static/base_app/js/base.js
+++ base_app/static/base_app/js/base.js
@@ -12,10 +12,10 @@ function search_open(){
 		searchBox.outerHTML = "<a class='respo-hover-none' style='height:1px' id='search_box'><input type='search' value='' placeholder='Search ...' style='height:0px; font-size:85%; margin-top:-6px;'></a>";
 
 		var searchBut = document.getElementById("srch_but");
-		searchBut.outerHTML = "<a href='#' class='respo-hover-white' style='height:0px' id='search_but' onclick='search_close()'><i class='fa fa-search fa-fw'></i></a>";
+		searchBut.outerHTML = "<a href='#' class='respo-hover-white' style='height:72px' id='search_but' onclick='search_close()'><i class='fa fa-search fa-fw'></i></a>";
 
 		var closeBut = document.getElementById("srch_close_but");
-		closeBut.outerHTML = "<a href='javascript:void(0)' class='respo-hover-white' style='height:0px' id='search_close_but' onclick='search_close()'><i class='fa fa-remove fa-fw'></i></a>";
+		closeBut.outerHTML = "<a href='javascript:void(0)' class='respo-hover-white' style='height:72px' id='search_close_but' onclick='search_close()'><i class='fa fa-remove fa-fw'></i></a>";
 	}
 }
 
